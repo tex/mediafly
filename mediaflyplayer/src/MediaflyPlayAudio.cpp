3b46012af1a77d1671bf94034da3a21de6aa1382
Milan Svoboda <milan.svoboda@centrum.cz>
2008-10-15 09:24:01 +0200

    Fix showing 'Episode \# of \#'.

--- mediaflyplayer/src/MediaflyPlayAudio.cpp
+++ mediaflyplayer/src/MediaflyPlayAudio.cpp
@@ -19,7 +19,11 @@ void MediaflyPlayAudio::show(const QModelIndex& index)
 	showTitleLabel->setText(m_index.data(MediaflyEpisodeModel::showTitleRole).toString());
 	showDetailsLabel->setHtml(m_index.data(MediaflyEpisodeModel::descriptionRole).toString());
 	episodeNameLabel->setText(m_index.data(MediaflyEpisodeModel::titleRole).toString());
-	numberOfEpisodesLabel->setText(m_index.row() + " / " + dynamic_cast<const MediaflyEpisodeModel *>(m_index.model())->totalRowCount());
+
+	QString episodesText = "Episode " + QString::number(m_index.row()) + " of " +
+		QString::number(dynamic_cast<const MediaflyEpisodeModel*>(m_index.model())->totalRowCount());
+
+	numberOfEpisodesLabel->setText(episodesText);
 
 	// TODO PLAY
 }
