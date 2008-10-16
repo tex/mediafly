41d899f7c1e385553cdb1f3a4bf3962789242e2d
Milan Svoboda <milan.svoboda@centrum.cz>
2008-10-16 14:41:48 +0200

    Rename member variables of Ui::MediaflyPlayAudio class to m_*.

--- mediaflyplayer/src/MediaflyPlayAudio.cpp
+++ mediaflyplayer/src/MediaflyPlayAudio.cpp
@@ -15,28 +15,25 @@ void MediaflyPlayAudio::show(const QModelIndex& index)
 	connect(m_index.model(), SIGNAL(refreshed()),
 	        this, SLOT(updateImage()));
 
-	iconLabel->setPixmap(m_index.data(MediaflyEpisodeModel::imageRole).value<QPixmap>());
-	showTitleLabel->setText(m_index.data(MediaflyEpisodeModel::showTitleRole).toString());
-	showDetailsLabel->setHtml(m_index.data(MediaflyEpisodeModel::descriptionRole).toString());
-	episodeNameLabel->setText(m_index.data(MediaflyEpisodeModel::titleRole).toString());
+	m_iconLabel->setPixmap(m_index.data(MediaflyEpisodeModel::imageRole).value<QPixmap>());
+	m_showTitleLabel->setText(m_index.data(MediaflyEpisodeModel::showTitleRole).toString());
+	m_showDetailsLabel->setHtml(m_index.data(MediaflyEpisodeModel::descriptionRole).toString());
+	m_episodeNameLabel->setText(m_index.data(MediaflyEpisodeModel::titleRole).toString());
 
 	QString episodesText = "Episode " + QString::number(m_index.row() + 1) + " of " +
 		QString::number(dynamic_cast<const MediaflyEpisodeModel*>(m_index.model())->totalRowCount());
 
-	numberOfEpisodesLabel->setText(episodesText);
-
-	// TODO PLAY
+	m_numberOfEpisodesLabel->setText(episodesText);
 }
 
 void MediaflyPlayAudio::updateImage()
 {
-	if (iconLabel->pixmap()->isNull()) {
-		iconLabel->setPixmap(m_index.data(MediaflyEpisodeModel::imageRole).value<QPixmap>());
+	if (m_iconLabel->pixmap()->isNull()) {
+		m_iconLabel->setPixmap(m_index.data(MediaflyEpisodeModel::imageRole).value<QPixmap>());
 	}
 }
 
 void MediaflyPlayAudio::hide()
 {
-	// TODO STOP PLAY?
 }
 
